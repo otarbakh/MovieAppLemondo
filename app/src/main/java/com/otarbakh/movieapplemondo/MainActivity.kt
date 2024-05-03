dd3b64c8477a89d0e74aa78a0482105af814259d
o.bakhtadze <o.bakhtadze@andersenlab.com>
2024-05-03 23:54:12 +0400

    Packaging not final

--- app/src/main/java/com/otarbakh/movieapplemondo/MainActivity.kt
+++ app/src/main/java/com/otarbakh/movieapplemondo/MainActivity.kt
@@ -33,11 +33,3 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MovieAppLemondoTheme {
-        Greeting("Android")
-    }
-}
