6c5c9bd4e9fca55a5a9571dfe2b7a7edded13e27
o.bakhtadze <o.bakhtadze@andersenlab.com>
2024-05-09 02:05:53 +0400

    README.md added

---
+++ README.md
@@ -0,0 +1,27 @@
+# Movies App
+
+![Movie App - Jetpack Compose](https://github.com/ahuamana/MoviesAppJetpackCompose/assets/60039961/a491680c-5bae-4464-b8ec-c781e6e5b60d)
+
+
+The Movies App is a sample Android application that allows users to browse and discover movies. It follows the principles of Clean Architecture, utilizes the MVVM (Model-View-ViewModel) architectural pattern, and is built using Jetpack Compose. The app also integrates Room for local database storage and utilizes Coroutines and Flow for asynchronous programming.
+
+## Movie Application for Lemondo
+
+## Features
+
+- Browse and search movies
+- View movies details ( title, description, release date, and rating)
+- Add movies to favorites
+
+
+## The App uses:
+
+- Android Jetpack Compose: jetpack compose reactive programming.
+- Kotlin Coroutines: For asynchronous programming.
+- Room Database for local data storage.
+- Retrofit
+- Gson: JSON serialization/deserialization library for parsing API responses.
+- Dagger Hilt: Dependency injection framework for Android.
+- Coil: Image loading library for displaying movie posters and images.
+
+
--- app/src/main/java/com/otarbakh/movieapplemondo/ui/presentation/composables/ErrorScreen.kt
+++ app/src/main/java/com/otarbakh/movieapplemondo/ui/presentation/composables/ErrorScreen.kt
@@ -1,44 +1,9 @@
 package com.otarbakh.movieapplemondo.ui.presentation.composables
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import com.airbnb.lottie.compose.LottieAnimation
-import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.LottieConstants
-import com.airbnb.lottie.compose.rememberLottieComposition
 import com.otarbakh.movieapplemondo.R
-
-@Composable
-fun ErrorScreen() {
-    val lottieLoading by rememberLottieComposition(
-        spec = LottieCompositionSpec.RawRes(R.raw.error_lottie)
-    )
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            LottieAnimation(
-                composition = lottieLoading,
-                modifier = Modifier.fillMaxWidth(1f),
-                iterations = LottieConstants.IterateForever
-            )
-        }
-    }
-}
 
 
 @Composable
